c4c0f3c3e7538849e8d5321ce6c0591a04abf2db
skew <skewskewskew@gmail.com>
2020-01-08 20:07:25 -0600

    Update note_delete_cmd.rb

--- plugins/utils/commands/note_delete_cmd.rb
+++ plugins/utils/commands/note_delete_cmd.rb
@@ -2,19 +2,38 @@ module AresMUSH
   module Utils
     class NoteDeleteCmd
       include CommandHandler
+
+# Adding a new "section" to notes ~ skew Nov-2019
+# Usage:
+# note/delete title=text - del note on yourself (note to self!)
+# note/delete player=title/txt - del note on a player, they can see it.
+# note/delete player/admin=title/txt - del an admin note on player, only staff can see it.
       
       attr_accessor :name, :target, :section
 
       def parse_args
-        args = cmd.parse_args(ArgParser.arg1_equals_optional_arg2)
-        if (args.arg2)
-          self.target = titlecase_arg(args.arg1)
-          self.name = titlecase_arg(args.arg2)
+        # First, break into 2 arguements, before+after the =
+        args = cmd.parse_args(ArgParser.arg1_equals_arg2)
+
+        # Next, check if it's an "admin" note in the first part. If so, staff-side.
+
+        if (args.arg1.include?("admin") )
+          self.target = args.arg1.first("/")
           self.section = 'admin'
+          self.name = titlecase_arg(args.arg2.first("/"))
+          self.text = trim_arg(args.arg2.rest("/"))
+
+        elsif (args.arg2.include?("/"))
+          self.target = titlecase_arg(args.arg1)
+          self.section = 'story'
+          self.name = titlecase_arg(args.arg2.first("/"))
+          self.text = trim_arg(args.arg2.rest("/"))
+
         else
           self.target = enactor_name
+          self.section = 'player' 
           self.name = titlecase_arg(args.arg1)
-          self.section = 'player'
+          self.text = trim_arg(args.arg2)
         end
       end
       
@@ -23,7 +42,7 @@ module AresMUSH
       end
       
       def check_is_allowed
-        return nil if self.target == enactor_name
+        return nil if self.section == 'player' && self.target == enactor_name
         return nil if Utils.can_access_notes?(enactor)
         return t('dispatcher.not_allowed')
       end
